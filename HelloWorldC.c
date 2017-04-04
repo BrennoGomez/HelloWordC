dcd11ef661e589f2a3dbfdbd074750c48777e01b
Breno Gomes da Silva <brennogomez9@gmail.com>
2017-04-03 23:52:11 -0300

    HelloWorldC
    Fazendo um Hello World na linguagem C.

--- HelloWorldC.c
+++ HelloWorldC.c
@@ -1,8 +1,9 @@
-#include "stdio.h"
+#include "stdio.h" //O comando #include permite que utilizemos o printf().
+                   //stdio.h é o arquivo da linguagem C onde estão funções básicas de entrada e saída. 
 
 int main() {
 
-  printf("Hello World!"); //função printf() terminal, saida padrao.
+  printf("Hello World!"); //função printf() terminal, saida padrão.
 
   return 0; //0 é o retorno que o programa executou com sucesso.
 }
